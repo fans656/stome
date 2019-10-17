afc2ce1e905af85c94e774062b2160e3c6000a84
fans656 <fans656@yahoo.com>
2019-10-17 10:06:52 +0800

    Frontend main layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Layout, Menu, Drawer, message } from 'antd';
+import 'antd/dist/antd.min.css';
+import './App.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <h1>Stome</h1>
+      <Layout style={{minHeight: '100%'}}>
+        {this.renderHeader()}
+        {this.renderExplorer()}
+      </Layout>
+    );
+  }
+
+  renderHeader = () => {
+    return (
+      <Layout.Header style={{height: '45px', paddingLeft: 0}}>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          style={{display: 'flex', lineHeight: '40px', padding: 0}}
+          selectedKeys={[]}
+        >
+          <Menu.Item>
+            <span className="logo">Stome</span>
+          </Menu.Item>
+        </Menu>
+      </Layout.Header>
+    );
+  }
+
+  renderExplorer = () => {
+    return (
+      <Layout.Content className="layout-content">
+      </Layout.Content>
     );
   }
 }
